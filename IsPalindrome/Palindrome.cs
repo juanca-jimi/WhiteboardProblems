c623bd4872f294536f929f15e350e581da3d7c28
Jimenez, Juan C <juancarlos.jimenez3@outlook.com>
2019-03-06 19:06:36 -0800

    Added palidrome problem project and unit test. 2 test cases failing; have to investigate further.

---
+++ IsPalindrome/Palindrome.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPalindrome
+{
+    public class Palindrome
+    {
+        //If we disregard everything besides a alphanumeric character and not taking into consideration case
+
+
+        static void Main(string[] args)
+        {
+        }
+        public static bool isPalindrome(string phrase)
+        {
+            phrase = phrase.ToLower();
+            string check1 = "";
+            string check2 = "";
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (isAlphaNumericChar(phrase[i]))
+                {
+                    check1 += phrase[i];
+                }
+            }
+            for (int i = check2.Length-1; i >=0; i++)
+            {
+                check2 += check2[i];
+            }
+            return (check1==check2) ?true:false;
+        }
+
+        public static bool isAlphaNumericChar(char character)
+        {
+            if (char.IsLetter(character) || char.IsNumber(character))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ IsPalindromeTests/PalindromeTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IsPalindrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPalindrome.Tests
+{
+    [TestClass()]
+    public class PalindromeTests
+    {
+        [TestMethod()]
+        public void isAlphaNumericCharTest_true1()
+        {
+            Assert.AreEqual(true, Palindrome.isAlphaNumericChar('j'));
+        }
+        [TestMethod()]
+        public void isAlphaNumericCharTest_true2()
+        {
+            Assert.AreEqual(true, Palindrome.isAlphaNumericChar('5'));
+        }
+        [TestMethod()]
+        public void isAlphaNumericCharTest_false1()
+        {
+            Assert.AreEqual(false, Palindrome.isAlphaNumericChar('='));
+        }
+        [TestMethod()]
+        public void isAlphaNumericCharTest_false2()
+        {
+            Assert.AreEqual(false, Palindrome.isAlphaNumericChar('?'));
+        }
+        [TestMethod()]
+        public void isPalindromeTest_true1()
+        {
+            Assert.AreEqual(true, Palindrome.isPalindrome("honeyenoh"));
+        }
+        [TestMethod()]
+        public void isPalindromeTest_true2()
+        {
+            Assert.AreEqual(true, Palindrome.isPalindrome("RAce____[][!@##$Car"));
+        }
+        [TestMethod()]
+        public void isPalindromeTest_false1()
+        {
+            Assert.AreEqual(false, Palindrome.isPalindrome("Firetruck"));
+        }
+        [TestMethod()]
+        public void isPalindromeTest_false2()
+        {
+            Assert.AreEqual(false, Palindrome.isPalindrome("racketBall[][][]"));
+        }
+    }
+}
