106021a9e02033f39b111c078bab2fe4c8620fb6
Jimenez, Juan C <juancarlos.jimenez3@outlook.com>
2019-03-08 21:42:16 -0800

    Fixed exceptions being thrown and added Rotating array problem

--- IsPalindrome/Palindrome.cs
+++ IsPalindrome/Palindrome.cs
@@ -9,28 +9,26 @@ namespace IsPalindrome
     public class Palindrome
     {
         //If we disregard everything besides a alphanumeric character and not taking into consideration case
-
-
         static void Main(string[] args)
         {
         }
         public static bool isPalindrome(string phrase)
         {
             phrase = phrase.ToLower();
-            string check1 = "";
-            string check2 = "";
+            string cleanPhrase = "";
+            string check = "";
             for (int i = 0; i < phrase.Length; i++)
             {
                 if (isAlphaNumericChar(phrase[i]))
                 {
-                    check1 += phrase[i];
+                    cleanPhrase += phrase[i];
                 }
             }
-            for (int i = check2.Length-1; i >=0; i++)
+            for (int i = cleanPhrase.Length-1; i >=0; i--)
             {
-                check2 += check2[i];
+                check += cleanPhrase[i];
             }
-            return (check1==check2) ?true:false;
+            return (cleanPhrase==check) ?true:false;
         }
 
         public static bool isAlphaNumericChar(char character)
---
+++ RotateArray/Rotate.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateArray
+{
+    class Rotate
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t RotatingArray.Rotate");
+            answer();
+        }
+        private static void prompt(int[] A, int[] B, int[] C)
+        {
+            Console.WriteLine("The values in Array A are");
+            displayArrValues(A);
+            Console.WriteLine("\nThe values in Array B are");
+            displayArrValues(B);
+            Console.WriteLine("\nThe values in Array C are");
+            displayArrValues(C);
+        }
+        private static void answer()
+        {
+            int[] A = { 0, 2, 4, 6, 8, 10 };
+            int[] B = { 1, 3, 5, 7, 9 };
+            int[] C = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+            prompt(A, B, C);
+            Console.WriteLine("\nplease select array to rotate!");
+            string Answer = Console.ReadLine().ToLower();
+            if (Answer == "a") RotArr(A);
+            else if (Answer == "b") RotArr(B);
+            else if (Answer == "c") RotArr(C);
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Please Try Again \n");
+                prompt(A, B, C);
+            }
+        }
+
+        private static void RotArr(int[] input)
+        {
+            Console.WriteLine("The values in your array are");
+            displayArrValues(input);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Would you like to rotate your array left or right? \nIf you wish to end enter: \"end\".");
+            string Direction = Console.ReadLine().ToLower();
+            if (Direction == "left")
+            {
+                int temporaryValue = input[0];
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    input[i] = input[i + 1];
+                }
+                input[input.Length - 1] = temporaryValue;
+                Console.Clear();
+                RotArr(input);
+            }
+            else if (Direction == "right")
+            {
+                int temporaryValue = input[input.Length - 1];
+                for (int i = input.Length - 1; i >= 1; i--)
+                {
+                    input[i] = input[i - 1];
+                }
+                input[0] = temporaryValue;
+                Console.Clear();
+                RotArr(input);
+            }
+            else if (Direction == "end")
+            {
+                Console.Clear();
+                Console.WriteLine("Thank you for your time.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("\tYou have entered the wrong input. \n\tPlease try again.\n");
+                RotArr(input);
+            }
+        }
+        private static void displayArrValues(int[] input)
+        {
+            foreach (int i in input) Console.Write(i + " ");
+        }
+
+    }
+}
