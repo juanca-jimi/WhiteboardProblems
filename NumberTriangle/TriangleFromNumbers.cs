87db8c11d789d3f20a8428cf2895d4153a4c4342
Jimenez, Juan C <juancarlos.jimenez3@outlook.com>
2019-04-05 08:35:40 -0700

    Cleaned TriangleFromNumbers and SumOfPrimes Projects

--- NumberTriangle/TriangleFromNumbers.cs
+++ NumberTriangle/TriangleFromNumbers.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumberTriangle
 {
@@ -10,21 +6,63 @@ namespace NumberTriangle
     {
         static void Main(string[] args)
         {
+            //Set as start up project
+            Console.WriteLine("This is your Number Triangle");
+
             createTriangle();
-            Console.ReadLine();
         }
         public static void createTriangle()
         {
+            //Keeps this in scope throughout the whole method
+            int triangleHeight = 0;
+
             Console.WriteLine("Input the number you wish to take your triangle up to");
-            int howHigh = Convert.ToInt32(Console.ReadLine());
-            for (int rowNumber = 1; rowNumber <= howHigh; rowNumber++)
+
+            try
             {
-                for (int digits = 1; digits <= rowNumber; digits++)
+                //Get triangle height from the user
+                triangleHeight = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //If we cannot convert to an integer
+            //Catch our exception and call the method recursively with the console cleared
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Please Try Again\n");
+                createTriangle();
+            }
+
+            
+            for (
+                //This is the number we're printing in every row
+                int rowNumberNumber = 1; 
+                //Don't stop til we reach the height of the triangle which is the number input by the user
+                rowNumberNumber <= triangleHeight; 
+                //increment by one
+                rowNumberNumber++)
+            {
+                //Nested loop
+                for (
+                    //This is the amount of digits we'll print out
+                    int digits = 1;
+                    
+                    //If we're at rowNumberNumber = 2 
+                    //print two digits of the rowNumberNumber
+                    digits <= rowNumberNumber;
+                    
+                    //increment by one
+                    digits++)
                 {
-                    Console.Write(rowNumber);
+                    //Don't start a new line and print the digit
+                    Console.Write(rowNumberNumber);
                 }
+
+                //Start a new line for a row number
                 Console.WriteLine();
             }
+
+            Console.ReadLine();
         }
     }
 }
--- SumOfPrimes/AddPrimes.cs
+++ SumOfPrimes/AddPrimes.cs
@@ -8,38 +8,72 @@ namespace SumOfPrimes
 {
     public class AddPrimes
     {
+        //TODO: write unit test
         static void Main(string[] args)
         {
+            //Set this as your startup project if not listed
             Console.WriteLine("This is your AddPrimes proj");
             Console.WriteLine(SumOfPrimes(200));
         }
         public static int SumOfPrimes(int input)
         {
+            //Our return integer that will get incremented
             int output = 0;
+
+            //our input is greater than 1 we'll hop int this loop that will add our primes
             if (input > 1)
             {
+
                 for (int i = 2; i <= input ; i++)
                 {
-                    output += (IsPrime(i)) ? i : 0;
+                    if (IsPrime(i))
+                    {
+                        //add it to our return integer
+                        output += i;
+                    }
+                    
+                    //This is not as efficient as new solution
+                    //Because we're not constantly added... just adding if it's prime
+                    //output += (IsPrime(i)) ? i : 0;
                 }
+
             }
+
             return output;
         }
 
+        //Our boolean that checks whether your input is prime
         public static bool IsPrime(int input)
         {
-            if (input == 1 || input == 0) return false;
-            if (input == 2) return true;
-            if (input > 1)
+            //1, 0 & Negative numbers are not Prime
+            if (input  <= 1 ) return false;
+
+            //Hard coded case to catch our 2 and return true
+            else if (input == 2) return true;
+
+            //If its greater than 2 we'll jump into this loop
+            else if (input > 2)
             {
-                for (int i = 2; i < input; i++)
+                for (
+                    //Begin dividing by 2 and if divisible by this integer then it's not prime
+                    int i = 2; 
+
+                    //To save runtime were going to stop at half because we don't need to check further
+                    i < input/2
+
+                    //Increment by 1
+                    ; i++)
                 {
+                    //If the remainder of our integer is perfectly divisible by an integer then it is not a prime
+                    //We'll return false in this case
                     if (input % i == 0)
                     {
                         return false;
                     }
                 }
             }
+
+            //If none of your cases are false then we'll return tre
             return true;
         }
     }
