dfcfb192f66f6170fc73a01a7767d54c27c746ec
Jimenez, Juan C <juancarlos.jimenez3@outlook.com>
2019-04-16 10:26:58 -0700

    Cleaned ReverseString with Comments

--- ReversingAString/ReverseString.cs
+++ ReversingAString/ReverseString.cs
@@ -10,15 +10,23 @@ namespace ReversingAString
     {
         static void Main(string[] args)
         {
-            Console.Write(revString("What are your goals?"));
+            //Test case.. Should return LQS
+            Console.Write(revString("SQL")); // LQS
+            Console.ReadLine();
         }
+
         public static string revString(string input)
         {
+            //Declaring our return string
             string finalString = "";
+
+            //Here we begin at the end of our String (char array) and go backwards up to our input[0]
+            //Then we add each index to the beginning of our return string
             for (int i = input.Length - 1; i > -1; i--)
             {
                 finalString += input[i];
             }
+
             return finalString;
         }
     }
